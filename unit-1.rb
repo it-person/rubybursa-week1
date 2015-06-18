2648319565188cc5ffef30cb20495bbeeca91660
Petro Koriakin <pkor@softserveinc.com>
2015-06-19 01:54:57 +0300

    some basic ifs

---
+++ unit-1.rb
@@ -0,0 +1,14 @@
+puts "Hello World!"
+
+a = 2
+res = if a > 5
+    "#{a} is bigger then 5"
+  elsif a < 5
+    "#{a} is smaller then 5"
+  else
+    "#{a} is equal 5"
+  end
+
+res2 = res
+
+puts res2
